605931a2779b245cfcc96210f63e176830d33c5a
Yohei Yasukawa <yohei@yasslab.jp>
2022-03-27 11:29:17 +0900

    Add active-support gem to manipulate time

--- wufoo2mattermost.rb
+++ wufoo2mattermost.rb
@@ -3,6 +3,7 @@
 require "net/http"
 require "uri"
 require "json"
+require 'active_support/time'
 
 WUFOO_API_USERNAME  = ENV['WUFOO_API_USERNAME']
 WUFOO_API_PASSWORD  = ENV['WUFOO_API_PASSWORD']
@@ -16,7 +17,7 @@ base_domain = "https://#{WUFOO_API_SUBDOMAIN}.wufoo.com"
 # 'form' can be replaced  with '[fields|entries].json]
 # https://wufoo.github.io/docs/?ruby#introduction
 #
-# Exec this to get Wufoo form IDs and set it to the variable
+# Exec this to get Wufoo form IDs and set it to the env variable.
 # uri = URI.parse(base_domain + '/api/v3/forms.json')
 
 # Get given form entries via API
@@ -27,9 +28,9 @@ request = Net::HTTP::Get.new(uri.request_uri)
 request.basic_auth(WUFOO_API_USERNAME, WUFOO_API_PASSWORD)
 
 # Make our request using https
-response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') {|http|
+response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
   http.request(request)
-}
+end
 
 # Print the output in a "pretty" json format
 puts JSON.pretty_generate(JSON[response.body])
