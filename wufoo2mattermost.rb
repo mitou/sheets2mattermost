565dd2deceadc0f36bdc238667bed32b5f47ff59
Yohei Yasukawa <yohei@yasslab.jp>
2022-03-27 12:03:23 +0900

    Sort fetched Wufoo entries in JST (GMT +0900)

--- wufoo2mattermost.rb
+++ wufoo2mattermost.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
-require "net/http"
-require "uri"
-require "json"
-require 'active_support/time'
+require 'net/http'
+require 'json'
+require 'time'
+#require 'active_support'
 
 WUFOO_API_USERNAME  = ENV['WUFOO_API_USERNAME']
 WUFOO_API_PASSWORD  = ENV['WUFOO_API_PASSWORD']
@@ -21,7 +21,7 @@ base_domain = "https://#{WUFOO_API_SUBDOMAIN}.wufoo.com"
 # uri = URI.parse(base_domain + '/api/v3/forms.json')
 
 # Get given form entries via API
-uri = URI.parse(base_domain + "/api/v3/forms/#{WUFOO_API_FORM_ID}/entries.json")
+uri = URI.parse(base_domain + "/api/v3/forms/#{WUFOO_API_FORM_ID}/entries.json?sort=EntryId&sortDirection=DESC")
 
 # Set up our request using the desired endpoint, and configure the basic auth
 request = Net::HTTP::Get.new(uri.request_uri)
@@ -34,3 +34,11 @@ end
 
 # Print the output in a "pretty" json format
 puts JSON.pretty_generate(JSON[response.body])
+
+# The following code is optimezed for Mitou Junior entry, so
+# this won't be worked. Please change each field on your own.
+
+JSON.parse(response.body)['Entries'].each do |entry|
+  pp Time.parse("#{entry['DateCreated']} +0900")
+  #binding.irb
+end
